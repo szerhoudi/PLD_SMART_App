b1b91f7a9c0f315636e8d23c8a4fc859a9b492ce
mathildebronnert <mathilde.bronnert@insa-lyon.fr>
2018-05-03 11:44:27 +0200

    clean code

--- src/pages/change-event/change-event.ts
+++ src/pages/change-event/change-event.ts
@@ -6,63 +6,53 @@ import { Storage } from '@ionic/storage';
 import 'rxjs/add/operator/map';
 
 /**
- * Generated class for the ChangeEventPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+* Generated class for the ChangeEventPage page.
+*
+* See https://ionicframework.com/docs/components/#navigation for more info on
+* Ionic pages and navigation.
+*/
 
 @IonicPage()
 @Component({
-  selector: 'page-change-event',
-  templateUrl: 'change-event.html',
+    selector: 'page-change-event',
+    templateUrl: 'change-event.html',
 })
 export class ChangeEventPage {
-  public posts: any = null;
-  public url = "https://s3.eu-west-3.amazonaws.com/pldsmart/rif.json";
-  
-  constructor(public alerCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams,  public http: Http, private storage: Storage) {
-    this.storage.get('url').then((val) => {
-    if(val!=null){
-        this.url = val;
-     }
-    });
-  this.http.get('https://s3.eu-west-3.amazonaws.com/pldsmart/listeEvent.json').map(res => res.json()).subscribe(data => {
-        this.posts = data;
-    })
-  }
+    public posts: any = null;
+    public url = "https://s3.eu-west-3.amazonaws.com/pldsmart/rif.json";
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ChangeEventPage');
-  }
+    constructor(public alerCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams,  public http: Http, private storage: Storage) {
+        this.storage.get('url').then((val) => {
+            if(val!=null){
+                this.url = val;
+            }
+        });
+        this.http.get('https://s3.eu-west-3.amazonaws.com/pldsmart/listeEvent.json').map(res => res.json()).subscribe(data => {
+            this.posts = data;
+        })
+    }
 
-  doConfirm(id) {
-    let confirm = this.alerCtrl.create({
-      title: 'Choose this event ?',
-      message: 'Do you really change to this event ? You will loose your favorites',
-      buttons: [
-        {
-          text: 'No',
-          handler: () => {
-            console.log('Disagree clicked');
-          }
-        },
-        {
-          text: 'Yes',
-          handler: () => {
-            console.log('Agree clicked');
-            this.storage.set('url', this.posts.Event[id].url);
-            console.log(this.posts.Event[id].url);
-            console.log(this.storage.get('url'));
-          this.storage.get('url').then((val) => {
-                console.log("val :",val);
-            });
-            this.navCtrl.setRoot(AccueilPage);
-          }
-        }
-      ]
-    });
-    confirm.present()
-  }
-    
+    doConfirm(id) {
+        let confirm = this.alerCtrl.create({
+            title: 'Choose this event ?',
+            message: 'Do you really change to this event ? You will loose your favorites',
+            buttons: [
+                {
+                    text: 'No',
+                    handler: () => {
+                    }
+                },
+                {
+                    text: 'Yes',
+                    handler: () => {
+                        this.storage.set('url', this.posts.Event[id].url);
+                        this.storage.get('url').then((val) => {
+                        });
+                        this.navCtrl.setRoot(AccueilPage);
+                    }
+                }
+            ]
+        });
+        confirm.present()
+    }
 }
