e75a224a50b93be9cef883ebd77f671a0f65dedd
mathildebronnert <mathilde.bronnert@insa-lyon.fr>
2018-05-03 09:55:22 +0200

    Add logo

--- src/pages/activities/activities.ts
+++ src/pages/activities/activities.ts
@@ -3,8 +3,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ActivitiesDetailPage} from '../activities-detail/activities-detail';
 import { Http } from '@angular/http';
 import { Storage } from '@ionic/storage';
-import { Rest } from '../../providers/rest';
-//import { Localisation} from '../localisation/localisation';
 
 /**
 * Generated class for the ActivitiesPage page.
